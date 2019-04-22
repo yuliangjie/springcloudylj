469567f7ea56cf862e0b2298f453cfc38f68be13
yuliangjie <306657941@qq.com>
2019-04-22 10:24:16 +0800

    eureka注册中心添加权限   spring-boot-starter-security

---
+++ eureka-server/src/main/java/com/zaltor/eurekaserver/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.zaltor.eurekaserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @date: 2019/4/22
+ * @author: win
+ */
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+
+}
