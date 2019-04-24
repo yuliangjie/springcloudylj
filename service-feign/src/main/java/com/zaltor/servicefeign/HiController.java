c55aaa0f8c8360543f560e5607bd46e9e5d6abef
yuliangjie <306657941@qq.com>
2019-04-24 09:45:25 +0800

    eureka注册中心添加权限   spring-boot-starter-security

--- service-feign/src/main/java/com/zaltor/servicefeign/HiController.java
+++ service-feign/src/main/java/com/zaltor/servicefeign/HiController.java
@@ -16,6 +16,7 @@ public class HiController {
     @Autowired
     SchedualServiceHi schedualServiceHi;
 
+
     @GetMapping(value = "/hi")
     public String sayHi(@RequestParam String name) {
         return schedualServiceHi.sayHiFromClientOne(name);
